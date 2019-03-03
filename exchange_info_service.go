a9c536da9640bc7a1264979c4c77333499924a3b
Hoàng Hà <halink0803@gmail.com>
2019-03-03 23:31:17 +0700

    add asset detail service as a part of exchange info service

--- exchange_info_service.go
+++ exchange_info_service.go
@@ -58,3 +58,42 @@ type Symbol struct {
 	IcebergAllowed     bool                     `json:"icebergAllowed"`
 	Filters            []map[string]interface{} `json:"filters"`
 }
+
+// AssetDetailService asset detail service
+type AssetDetailService struct {
+	c *Client
+}
+
+// Do send request
+func (ads *AssetDetailService) Do(ctx context.Context, opts ...RequestOption) (res *AssetDetail, err error) {
+	r := &request{
+		method:   "GET",
+		endpoint: "/wapi/v3/assetDetail.html",
+		secType:  secTypeNone,
+	}
+	data, err := s.c.callAPI(ctx, r, opts...)
+	if err != nil {
+		return nil, err
+	}
+	res = new(AssetDetail)
+	err = json.Unmarshal(data, res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+//AssetDetail asset detail
+type AssetDetail struct {
+	AssetDetail map[string]DetailOfAsset `json:"assetDetail"`
+}
+
+//DetailOfAsset return detail of an asset
+type DetailOfAsset struct {
+	MinWithdrawAmount float64 `json:"minWithdrawAmount"`
+	DepositStatus     bool    `json:"depositStatus"`
+	WithdrawFee       float64 `json:"withdrawFee"`
+	WithdrawStatus    bool    `json:"withdrawStatus"`
+	DepositTip        string  `json:"depositTip"`
+}
